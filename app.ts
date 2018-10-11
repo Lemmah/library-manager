266ed5c051fb068fc6d8fbdc8f077664e9fb9a6c
Lemmah <lemayiannakolah@gmail.com>
2018-10-11 10:51:28 +0300

    style(app): use enums, arrays and tuples
    - enums allow us to define a set of named constants.
    - tuples are arrays with known types, almost immutable.
    - arrays are just like they are in JavaScript.
    
    [Delivers #arraysTuplesEnum]

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
 function GetAllBooks() {
+
   let books = [
-    {title: 'Random Book', author: 'James Joyes', available: false},
-    {title: 'Random Book', author: 'James Joyes', available: false},
-    {title: 'Random Book', author: 'James Joyes', available: true}
+    {id: 1, title: 'Random Book', author: 'James Joyes', available: false, category: Category.Fiction},
+    {id: 2, title: 'Random Book', author: 'James Joyes', available: false, category: Category.Poetry },
+    {id: 3, title: 'Random Book', author: 'James Joyes', available: true, category: Category.Biography}
   ];
   return books;
 }
@@ -23,4 +24,27 @@ function LogFirstAvailable(books): void {
   console.log(`First Available: ${firstAvailable}`)
 }
 
-LogFirstAvailable(GetAllBooks());
+enum Category { Biography, Poetry, Fiction, History, Children };
+
+function GetBookTitleByCategory(categoryFilter: Category): Array<string> {
+  console.log(`Getting books in category: ${Category[categoryFilter]}`);
+  const allBooks = GetAllBooks();
+  const filteredTitles: string[] = [];
+  for (let currentBook of allBooks) {
+    if(currentBook.category === categoryFilter){
+      filteredTitles.push(currentBook.title);
+    }
+  }
+
+  return filteredTitles;
+}
+
+function LogBookTitles(titles: string[]): void {
+  for(let title of titles) {
+    console.log(title);
+  }
+}
+
+
+const poetryBooks = GetBookTitleByCategory(Category.Poetry);
+LogBookTitles(poetryBooks);
